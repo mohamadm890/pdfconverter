cc6ada1cb5d4dfe55851dd1c387cfd7ef79a9205
mohamadm890 <94972216+mohamadm890@users.noreply.github.com>
2024-09-19 10:15:20 -0700

    Update Processing.js

--- src/pages/Processing.js
+++ src/pages/Processing.js
@@ -14,13 +14,14 @@ export default function Processing() {
   }, 5000);
   return () => clearTimeout(timer);
   }, []) 
+
   return (
     <main
       className={`flex justify-center flex-col  ${inter.className}`}
     >
             <Header />
             <div className={'flex flex-col justify-center m-auto text-center mt-12 '}>
-                <h1 className={'mb-8'}>Please wait...</h1>
+                <h1 className={'mb-8 '}>Please wait...</h1>
                 <div className={' w-40 h-40 rounded-lg shadow-lg shadow-red-200/50'}></div>
                 <h2 className={'mt-4'}>In progress...</h2>
             </div>
