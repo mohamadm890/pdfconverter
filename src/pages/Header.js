9c50341e7c2c821d9f3cf6ca6e8d74eb18b05bd6
mohamadm890 <94972216+mohamadm890@users.noreply.github.com>
2024-09-19 10:25:49 -0700

    Update Header.js

--- src/pages/Header.js
+++ src/pages/Header.js
@@ -1,15 +1,25 @@
 import { Inter } from "next/font/google";
-
+import Link from "next/link";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Header() {
   
   return (
     <main
-      className={`flex justify-center p-4  ${inter.className}`}
+      className={`flex sp p-4  justify-between mx-2.5  ${inter.className}`}
     >
+
     <h1 className="text-2xl">Lion<span className="text-red-600 font-bold  ">Docs</span></h1>
-   
-    </main>
+   <div className="flex items-center gap-4  	 ">
+    <Link href="/Login" className="text-slate-600 font-bold"> 
+        Login 
+      </Link>
+    <div className="flex items-center gap-4 p-2 bg-red-100 rounded-md text-white font-medium	">
+    <Link href="/SignUp" className="text-red-500 font-medium	"> 
+        Sign up 
+      </Link>
+      </div>
+</div>
+          </main>
   );
 }
