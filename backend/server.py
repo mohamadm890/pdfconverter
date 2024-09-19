8d443c0affa135476942ac51bcb2b06550507cf4
mohamadm890 <94972216+mohamadm890@users.noreply.github.com>
2024-09-19 08:08:16 -0700

    Update server.py

--- backend/server.py
+++ backend/server.py
@@ -165,4 +165,5 @@ def trail():
     return jsonify({"message": "File uploaded and processed successfully"}), 200
 
 if __name__ == '__main__':
-    app.run(debug=True)
+     port = int(os.environ.get('PORT', 5000))  # Default to 5000 if not set
+     app.run(host='0.0.0.0', port=port)
